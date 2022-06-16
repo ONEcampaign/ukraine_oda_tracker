2c6c475817162c6b5f1067eb8803e411b38797f6
Jorge Rivera <jorge.rivera@one.org>
2022-06-16 16:22:28 -0600

    update

--- update.py
+++ update.py
@@ -1,6 +1,16 @@
 from scripts.create_table import get_data, SHEETS, build_table
 from scripts.config import PATHS
+from datetime import datetime
+from csv import writer
 
+def last_updated():
+    """Appends the date of last run to a csv"""
+
+    with open(PATHS.output + r"/updates.csv", "a+", newline="") as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow([datetime.today()])
 
 if __name__ == "__main__":
     raw_data = get_data(pages_dict=SHEETS)
