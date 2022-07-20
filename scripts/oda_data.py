c4bc7bfef5081771eaeb3bb1fc96b6be64057698
Jorge Rivera <jorge.rivera@one.org>
2022-07-20 16:05:47 +0200

    data update

--- scripts/oda_data.py
+++ scripts/oda_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pydeflate
+from country_converter import country_converter
 
 from scripts import config
 
@@ -18,6 +19,107 @@ def read_idrc():
         .filter(["year", "donor_name", "value"], axis=1)
         .rename(columns={"value": "idrc"})
     )
+
+
+def read_gni():
+    return (
+        pd.read_csv(f"{config.PATHS.data}/gni.csv")
+        .filter(["year", "donor_name", "value"], axis=1)
+        .rename(columns={"value": "gni"})
+    )
+
+
+def read_idrc_estimates():
+    url = (
+        "https://docs.google.com/spreadsheets/d/e/2PACX-1vSqAIxjSZ78fE93CP1K9K0t8rL"
+        "M2wi0z_nc60ezrUeDEIOPz-vr01SmmS_5nNnq_uPE0dM26m0V3rQK/pub?gid=1887834724"
+        "&single=true&output=csv"
+    )
+    return (
+        pd.read_csv(url)
+        .assign(
+            donor_name=lambda d: country_converter.convert(d.iso_code, to="short_name"),
+            year=2022,
+        )
+        .rename(columns={"estimated_idrc": "idrc"})
+        .drop("iso_code", axis=1)
+    )
+
+
+def _pop_groups(list_: list, group_size: int) -> tuple[list, ...]:
+    from collections import deque
+
+    list_ = deque(list_)
+    groups = []
+    while len(list_) > 0:
+        if len(list_) >= group_size:
+            countries = [list_.popleft() for i in range(group_size)]
+        else:
+            countries = [list_.popleft() for i in range(len(list_))]
+
+        groups.append(countries)
+
+    return tuple(groups)
+
+
+def idrc_oda_chart() -> None:
+
+    # Read the different datasets that are needed for the chart
+    idrc_est = read_idrc_estimates()
+    idrc = pd.concat([read_idrc(), idrc_est], ignore_index=True)
+    oda = read_oda()
+    gni = read_gni()
+
+    # Assign the 2021 GNI value to 2022
+    gni22 = gni.copy(deep=True).loc[lambda d: d.year == 2021].assign(year=2022)
+    gni = pd.concat([gni, gni22], ignore_index=True)
+
+    idrc, oda, gni = [
+        d.loc[d.year.isin([2012, 2016, 2021, 2022])]
+        .sort_values(["year", "donor_name"])
+        .reset_index(drop=True)
+        for d in [idrc, oda, gni]
+    ]
+
+    p1_countries = [
+        "Canada",
+        "United States",
+        "France",
+        "Germany",
+        "Italy",
+        "United Kingdom",
+    ]
+
+    other_countries = [c for c in idrc.donor_name.unique() if c not in p1_countries]
+
+    chart_pages = _pop_groups(other_countries, 6)
+    chart_pages = [p1_countries] + [*chart_pages]
+
+    def __export_df_page(page: int, page_countries: list):
+        _ = (
+            idrc.merge(oda, on=["year", "donor_name"], how="outer")
+            .merge(gni, on=["year", "donor_name"], how="outer")
+            .sort_values(["year", "donor_name"])
+            .loc[lambda d: d.donor_name.isin(page_countries)]
+            .reset_index(drop=True)
+            .assign(
+                idrc_gni=lambda d: round(100 * d.idrc / d.gni, 3),
+                oda_gni=lambda d: round(100 * d.total_oda / d.gni, 2),
+            )
+            .rename(
+                columns={
+                    "donor_name": "Donor",
+                    "idrc": "In-Donor Refugee Costs",
+                    "total_oda": "Total ODA",
+                    "idrc_gni": "IDRC as a share of GNI",
+                    "oda_gni": "ODA as a share of GNI",
+                    "gni": "GNI",
+                }
+            )
+        ).to_csv(f"{config.PATHS.output}/idrc_oda_chart_{page}.csv", index=False)
+
+    for page_, list_ in enumerate(chart_pages):
+        __export_df_page(page=page_, page_countries=list_)
 
 
 def idrc_as_share():
@@ -74,5 +176,7 @@ if __name__ == "__main__":
     share = idrc_as_share()
     share.to_csv(config.PATHS.output + "/idrc_share.csv", index=False)
 
-    idrc_constant = idrc_constant_wide()
-    idrc_constant.to_csv(config.PATHS.output + "/idrc_constant.csv", index=False)
+    idrc_const = idrc_constant_wide()
+    idrc_const.to_csv(config.PATHS.output + "/idrc_constant.csv", index=False)
+
+    idrc_oda_chart()
